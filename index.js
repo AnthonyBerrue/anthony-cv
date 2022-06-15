c9a19e8391cf7d8cb804d3f4907f7634949012bd
AnthonyBerrue <django49000@gmail.com>
2022-06-15 18:35:32 +0200

    backup code

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// Import builtin NodeJS modules to instantiate the service
-const https = require("https");
-const fs = require("fs");
-
+const http = require('http');
 require('dotenv').config();
 const debug = require('debug')('app:server');
 const app = require('./app');
@@ -9,16 +6,8 @@ const app = require('./app');
 
 const port = process.env.PORT ?? 3000;
 
-const server = https.createServer(
+const server = http.createServer(app);
 
-    // Provide the private and public key to the server by reading each
-// file's content with the readFileSync() method.    
-    {
-        key: fs.readFileSync("key.pem"),
-        cert: fs.readFileSync("cert.pem"),
-    },
-    app
-);
 
 server.listen(port, () => {
     debug(`Listening on ${port}`);
