e4a98c22b3aeaec8ac735d9f6903e5731d80c41a
AnthonyBerrue <django49000@gmail.com>
2022-06-15 18:29:58 +0200

    Create an HTTPS NodeJS

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const http = require('http');
+// Import builtin NodeJS modules to instantiate the service
+const https = require("https");
+const fs = require("fs");
+
 require('dotenv').config();
 const debug = require('debug')('app:server');
 const app = require('./app');
@@ -6,8 +9,16 @@ const app = require('./app');
 
 const port = process.env.PORT ?? 3000;
 
-const server = http.createServer(app);
+const server = https.createServer(
 
+    // Provide the private and public key to the server by reading each
+// file's content with the readFileSync() method.    
+    {
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("cert.pem"),
+    },
+    app
+);
 
 server.listen(port, () => {
     debug(`Listening on ${port}`);
