14764cb5f1731bb9b862adb5b13aa5bfd5481047
Anthony Berrué <django49000@gmail.com>
2025-11-03 11:11:04 +0100

    try13

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,10 @@
-export default function robots() {
+import type { MetadataRoute } from 'next';
+
+export const dynamic = 'force-static';
+
+export default function robots(): MetadataRoute.Robots {
     return {
         rules: { userAgent: '*', allow: '/' },
-        sitemap: 'https://anthonyberrue.github.io/anthony-cv/sitemap.xml'
+        sitemap: 'https://anthonyberrue.github.io/anthony-cv/sitemap.xml',
     };
 }
